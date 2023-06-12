36ac10c44db9ac6fe8c2b68af3656085c3c2d306
zebin-dm <zebin.cai@deepmirror.com>
2023-06-12 16:53:13 +0800

    modify metric

--- test_ace.py
+++ test_ace.py
@@ -1,27 +1,23 @@
 #!/usr/bin/env python3
 # Copyright © Niantic, Inc. 2022.
-
-import argparse
-import logging
+import cv2
 import math
 import time
-from distutils.util import strtobool
-from pathlib import Path
-
-import cv2
-import numpy as np
 import torch
+import argparse
+import numpy as np
+
+from pathlib import Path
+from loguru import logger
+from distutils.util import strtobool
 from torch.cuda.amp import autocast
 from torch.utils.data import DataLoader
 
 import dsacstar
+import ace_vis_util as vutil
 from ace_network import Regressor
 from dataset import CamLocDataset
-
-import ace_vis_util as vutil
 from ace_visualizer import ACEVisualizer
-
-_logger = logging.getLogger(__name__)
 
 
 def _strtobool(x):
@@ -29,69 +25,126 @@ def _strtobool(x):
 
 
 if __name__ == '__main__':
-    # Setup logging.
-    logging.basicConfig(level=logging.INFO)
-
     parser = argparse.ArgumentParser(
         description='Test a trained network on a specific scene.',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    parser.add_argument('scene', type=Path,
-                        help='path to a scene in the dataset folder, e.g. "datasets/Cambridge_GreatCourt"')
+    parser.add_argument(
+        'scene',
+        type=Path,
+        help=
+        'path to a scene in the dataset folder, e.g. "datasets/Cambridge_GreatCourt"'
+    )
 
-    parser.add_argument('network', type=Path, help='path to a network trained for the scene (just the head weights)')
+    parser.add_argument(
+        'network',
+        type=Path,
+        help='path to a network trained for the scene (just the head weights)')
 
-    parser.add_argument('--encoder_path', type=Path, default=Path(__file__).parent / "ace_encoder_pretrained.pt",
+    parser.add_argument('--encoder_path',
+                        type=Path,
+                        default=Path(__file__).parent /
+                        "ace_encoder_pretrained.pt",
                         help='file containing pre-trained encoder weights')
 
-    parser.add_argument('--session', '-sid', default='',
+    parser.add_argument('--session',
+                        '-sid',
+                        default='',
                         help='custom session name appended to output files, '
-                             'useful to separate different runs of a script')
+                        'useful to separate different runs of a script')
 
-    parser.add_argument('--image_resolution', type=int, default=480, help='base image resolution')
+    parser.add_argument('--image_resolution',
+                        type=int,
+                        default=480,
+                        help='base image resolution')
 
     # ACE is RGB-only, no need for this param.
     # parser.add_argument('--mode', '-m', type=int, default=1, choices=[1, 2], help='test mode: 1 = RGB, 2 = RGB-D')
 
     # DSACStar RANSAC parameters. ACE Keeps them at default.
-    parser.add_argument('--hypotheses', '-hyps', type=int, default=64,
-                        help='number of hypotheses, i.e. number of RANSAC iterations')
+    parser.add_argument(
+        '--hypotheses',
+        '-hyps',
+        type=int,
+        default=64,
+        help='number of hypotheses, i.e. number of RANSAC iterations')
 
-    parser.add_argument('--threshold', '-t', type=float, default=10,
-                        help='inlier threshold in pixels (RGB) or centimeters (RGB-D)')
+    parser.add_argument(
+        '--threshold',
+        '-t',
+        type=float,
+        default=10,
+        help='inlier threshold in pixels (RGB) or centimeters (RGB-D)')
 
-    parser.add_argument('--inlieralpha', '-ia', type=float, default=100,
-                        help='alpha parameter of the soft inlier count; controls the softness of the '
-                             'hypotheses score distribution; lower means softer')
+    parser.add_argument(
+        '--inlieralpha',
+        '-ia',
+        type=float,
+        default=100,
+        help=
+        'alpha parameter of the soft inlier count; controls the softness of the '
+        'hypotheses score distribution; lower means softer')
 
-    parser.add_argument('--maxpixelerror', '-maxerrr', type=float, default=100,
-                        help='maximum reprojection (RGB, in px) or 3D distance (RGB-D, in cm) error when checking '
-                             'pose consistency towards all measurements; error is clamped to this value for stability')
+    parser.add_argument(
+        '--maxpixelerror',
+        '-maxerrr',
+        type=float,
+        default=100,
+        help=
+        'maximum reprojection (RGB, in px) or 3D distance (RGB-D, in cm) error when checking '
+        'pose consistency towards all measurements; error is clamped to this value for stability'
+    )
 
     # Params for the visualization. If enabled, it will slow down relocalisation considerably. But you get a nice video :)
-    parser.add_argument('--render_visualization', type=_strtobool, default=False,
+    parser.add_argument('--render_visualization',
+                        type=_strtobool,
+                        default=False,
                         help='create a video of the mapping process')
 
-    parser.add_argument('--render_target_path', type=Path, default='renderings',
-                        help='target folder for renderings, visualizer will create a subfolder with the map name')
+    parser.add_argument(
+        '--render_target_path',
+        type=Path,
+        default='renderings',
+        help=
+        'target folder for renderings, visualizer will create a subfolder with the map name'
+    )
 
-    parser.add_argument('--render_flipped_portrait', type=_strtobool, default=False,
-                        help='flag for wayspots dataset where images are sideways portrait')
+    parser.add_argument(
+        '--render_flipped_portrait',
+        type=_strtobool,
+        default=False,
+        help='flag for wayspots dataset where images are sideways portrait')
 
-    parser.add_argument('--render_sparse_queries', type=_strtobool, default=False,
-                        help='set to true if your queries are not a smooth video')
+    parser.add_argument(
+        '--render_sparse_queries',
+        type=_strtobool,
+        default=False,
+        help='set to true if your queries are not a smooth video')
 
-    parser.add_argument('--render_pose_error_threshold', type=int, default=20,
-                        help='pose error threshold for the visualisation in cm/deg')
+    parser.add_argument(
+        '--render_pose_error_threshold',
+        type=int,
+        default=20,
+        help='pose error threshold for the visualisation in cm/deg')
 
-    parser.add_argument('--render_map_depth_filter', type=int, default=10,
-                        help='to clean up the ACE point cloud remove points too far away')
+    parser.add_argument(
+        '--render_map_depth_filter',
+        type=int,
+        default=10,
+        help='to clean up the ACE point cloud remove points too far away')
 
-    parser.add_argument('--render_camera_z_offset', type=int, default=4,
-                        help='zoom out of the scene by moving render camera backwards, in meters')
+    parser.add_argument(
+        '--render_camera_z_offset',
+        type=int,
+        default=4,
+        help=
+        'zoom out of the scene by moving render camera backwards, in meters')
 
-    parser.add_argument('--render_frame_skip', type=int, default=1,
-                        help='skip every xth frame for long and dense query sequences')
+    parser.add_argument(
+        '--render_frame_skip',
+        type=int,
+        default=1,
+        help='skip every xth frame for long and dense query sequences')
 
     opt = parser.parse_args()
 
@@ -109,19 +162,20 @@ if __name__ == '__main__':
         mode=0,  # Default for ACE, we don't need scene coordinates/RGB-D.
         image_height=opt.image_resolution,
     )
-    _logger.info(f'Test images found: {len(testset)}')
+    logger.info(f'Test images found: {len(testset)}')
 
     # Setup dataloader. Batch size 1 by default.
     testset_loader = DataLoader(testset, shuffle=False, num_workers=6)
 
     # Load network weights.
     encoder_state_dict = torch.load(encoder_path, map_location="cpu")
-    _logger.info(f"Loaded encoder from: {encoder_path}")
+    logger.info(f"Loaded encoder from: {encoder_path}")
     head_state_dict = torch.load(head_network_path, map_location="cpu")
-    _logger.info(f"Loaded head weights from: {head_network_path}")
+    logger.info(f"Loaded head weights from: {head_network_path}")
 
     # Create regressor.
-    network = Regressor.create_from_split_state_dict(encoder_state_dict, head_state_dict)
+    network = Regressor.create_from_split_state_dict(encoder_state_dict,
+                                                     head_state_dict)
 
     # Setup for evaluation.
     network = network.to(device)
@@ -132,10 +186,10 @@ if __name__ == '__main__':
     scene_name = scene_path.name
     # This will contain aggregate scene stats (median translation/rotation errors, and avg processing time per frame).
     test_log_file = output_dir / f'test_{scene_name}_{opt.session}.txt'
-    _logger.info(f"Saving test aggregate statistics to: {test_log_file}")
+    logger.info(f"Saving test aggregate statistics to: {test_log_file}")
     # This will contain each frame's pose (stored as quaternion + translation) and errors.
     pose_log_file = output_dir / f'poses_{scene_name}_{opt.session}.txt'
-    _logger.info(f"Saving per-frame poses and errors to: {pose_log_file}")
+    logger.info(f"Saving per-frame poses and errors to: {pose_log_file}")
 
     # Setup output files.
     test_log = open(test_log_file, 'w', 1)
@@ -158,13 +212,13 @@ if __name__ == '__main__':
     # Generate video of training process
     if opt.render_visualization:
         # infer rendering folder from map file name
-        target_path = vutil.get_rendering_target_path(
-            opt.render_target_path,
-            opt.network)
-        ace_visualizer = ACEVisualizer(target_path,
-                                       opt.render_flipped_portrait,
-                                       opt.render_map_depth_filter,
-                                       reloc_vis_error_threshold=opt.render_pose_error_threshold)
+        target_path = vutil.get_rendering_target_path(opt.render_target_path,
+                                                      opt.network)
+        ace_visualizer = ACEVisualizer(
+            target_path,
+            opt.render_flipped_portrait,
+            opt.render_map_depth_filter,
+            reloc_vis_error_threshold=opt.render_pose_error_threshold)
 
         # we need to pass the training set in case the visualiser has to regenerate the map point cloud
         trainset = CamLocDataset(
@@ -202,8 +256,10 @@ if __name__ == '__main__':
             scene_coordinates_B3HW = scene_coordinates_B3HW.float().cpu()
 
             # Each frame is processed independently.
-            for frame_idx, (scene_coordinates_3HW, gt_pose_44, intrinsics_33, frame_path) in enumerate(
-                    zip(scene_coordinates_B3HW, gt_pose_B44, intrinsics_B33, filenames)):
+            for frame_idx, (scene_coordinates_3HW, gt_pose_44, intrinsics_33,
+                            frame_path) in enumerate(
+                                zip(scene_coordinates_B3HW, gt_pose_B44,
+                                    intrinsics_B33, filenames)):
 
                 # Extract focal length and principal point from the intrinsics matrix.
                 focal_length = intrinsics_33[0, 0].item()
@@ -233,7 +289,8 @@ if __name__ == '__main__':
                 )
 
                 # Calculate translation error.
-                t_err = float(torch.norm(gt_pose_44[0:3, 3] - out_pose[0:3, 3]))
+                t_err = float(torch.norm(gt_pose_44[0:3, 3] -
+                                         out_pose[0:3, 3]))
 
                 # Rotation error.
                 gt_R = gt_pose_44[0:3, 0:3].numpy()
@@ -245,14 +302,16 @@ if __name__ == '__main__':
                 # Extract the angle.
                 r_err = np.linalg.norm(r_err) * 180 / math.pi
 
-                _logger.info(f"Rotation Error: {r_err:.2f}deg, Translation Error: {t_err * 100:.1f}cm")
+                # logger.info(
+                #     f"Rotation Error: {r_err:.2f}deg, Translation Error: {t_err * 100:.1f}cm"
+                # )
 
                 if ace_visualizer is not None:
                     ace_visualizer.render_reloc_frame(
                         query_pose=gt_pose_44.numpy(),
                         query_file=frame_path,
                         est_pose=out_pose.numpy(),
-                        est_error=max(r_err, t_err*100),
+                        est_error=max(r_err, t_err * 100),
                         sparse_query=opt.render_sparse_queries)
 
                 # Save the errors.
@@ -285,10 +344,11 @@ if __name__ == '__main__':
                 q_xyz = math.sin(angle * 0.5) * axis
 
                 # Write to output file. All in a single line.
-                pose_log.write(f"{frame_name} "
-                               f"{q_w} {q_xyz[0].item()} {q_xyz[1].item()} {q_xyz[2].item()} "
-                               f"{t[0]} {t[1]} {t[2]} "
-                               f"{r_err} {t_err} {inlier_count}\n")
+                pose_log.write(
+                    f"{frame_name} "
+                    f"{q_w} {q_xyz[0].item()} {q_xyz[1].item()} {q_xyz[2].item()} "
+                    f"{t[0]} {t[1]} {t[2]} "
+                    f"{r_err} {t_err} {inlier_count}\n")
 
             avg_batch_time += time.time() - batch_start_time
             num_batches += 1
@@ -312,17 +372,15 @@ if __name__ == '__main__':
     pct2 = pct2 / total_frames * 100
     pct1 = pct1 / total_frames * 100
 
-    _logger.info("===================================================")
-    _logger.info("Test complete.")
-
-    _logger.info('Accuracy:')
-    _logger.info(f'\t10cm/5deg: {pct10_5:.1f}%')
-    _logger.info(f'\t5cm/5deg: {pct5:.1f}%')
-    _logger.info(f'\t2cm/2deg: {pct2:.1f}%')
-    _logger.info(f'\t1cm/1deg: {pct1:.1f}%')
-
-    _logger.info(f"Median Error: {median_rErr:.1f}deg, {median_tErr:.1f}cm")
-    _logger.info(f"Avg. processing time: {avg_time * 1000:4.1f}ms")
+    print_text = f"""
+===================================================
+Test complete.
+Accuracy:
+10cm/5deg: {pct10_5:.1f}, 5cm/5deg: {pct5:.1f}, 2cm/2deg: {pct2:.1f}, 1cm/1deg: {pct1:.1f}
+Median Error: {median_rErr:.1f}deg, {median_tErr:.1f}cm
+Avg. processing time: {avg_time * 1000:4.1f}ms
+"""
+    logger.info(print_text)
 
     # Write to the test log file as well.
     test_log.write(f"{median_rErr} {median_tErr} {avg_time}\n")
