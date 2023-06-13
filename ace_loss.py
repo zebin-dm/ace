29de36a23a82c91d8510eb2d4bef8c66a69ce7b4
zebin-dm <zebin.cai@deepmirror.com>
2023-06-13 18:31:43 +0800

    update

--- ace_loss.py
+++ ace_loss.py
@@ -23,12 +23,7 @@ class ReproLoss:
     - l1+logl1: Similar to the above, but using log L1 for pixels with high reprojection error.
     """
 
-    def __init__(self,
-                 total_iterations,
-                 soft_clamp,
-                 soft_clamp_min,
-                 loss_type='dyntanh',
-                 circle_schedule=True):
+    def __init__(self, total_iterations, soft_clamp, soft_clamp_min, loss_type='dyntanh', circle_schedule=True):
 
         self.total_iterations = total_iterations
         self.soft_clamp = soft_clamp
@@ -52,8 +47,7 @@ class ReproLoss:
                 schedule_weight = 1 - np.sqrt(1 - schedule_weight**2)
 
             # Compute the weight to use in the tanh loss.
-            loss_weight = (
-                1 - schedule_weight) * self.soft_clamp + self.soft_clamp_min
+            loss_weight = (1 - schedule_weight) * self.soft_clamp + self.soft_clamp_min
 
             # Compute actual loss.
             return weighted_tanh(repro_errs_b1N, loss_weight)
@@ -67,8 +61,7 @@ class ReproLoss:
             # L1 loss on pixels with small errors and sqrt for the others.
             softclamp_mask_b1 = repro_errs_b1N > self.soft_clamp
             loss_l1 = repro_errs_b1N[~softclamp_mask_b1].sum()
-            loss_sqrt = torch.sqrt(self.soft_clamp *
-                                   repro_errs_b1N[softclamp_mask_b1]).sum()
+            loss_sqrt = torch.sqrt(self.soft_clamp * repro_errs_b1N[softclamp_mask_b1]).sum()
 
             return loss_l1 + loss_sqrt
 
@@ -76,8 +69,6 @@ class ReproLoss:
             # l1+logl1: same as above, but use log(L1) for pixels with a larger error.
             softclamp_mask_b1 = repro_errs_b1N > self.soft_clamp
             loss_l1 = repro_errs_b1N[~softclamp_mask_b1].sum()
-            loss_logl1 = torch.log(1 +
-                                   (self.soft_clamp *
-                                    repro_errs_b1N[softclamp_mask_b1])).sum()
+            loss_logl1 = torch.log(1 + (self.soft_clamp * repro_errs_b1N[softclamp_mask_b1])).sum()
 
             return loss_l1 + loss_logl1
